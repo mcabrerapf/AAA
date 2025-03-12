d6c990c06a6c28287418341a6faa8e1cc8bfed20
Manuel <mcabrerapf@gmail.com>
2025-03-12 22:37:04 +0100

    changed some images

--- components/LogoSmall.js
+++ components/LogoSmall.js
@@ -1,7 +1,7 @@
 export default function LogoSmall() {
     return ` 
         <div class="logo-small">
-            <img src="/public/Animal_Antics_Logo_Colors_1000.webp" />
+            <img src="./public/Animal_Antics_Logo_Colors_1000.webp" />
         </div>
     `;
 }
