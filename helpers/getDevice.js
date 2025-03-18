330850f3d30099592a0b81324a8fd6bc2d851089
Manuel <mcabrerapf@gmail.com>
2025-03-18 21:17:13 +0100

    innwer width check

--- helpers/getDevice.js
+++ helpers/getDevice.js
@@ -1,21 +1,30 @@
 export default function getDeviceType() {
-    let width;
-
-    try {
-        if (window.top !== window.self) {
-            width = window.top.innerWidth;
-        } else {
-            width = window.innerWidth;
-        }
-    } catch (e) {
-        width = window.innerWidth;
-    }
-
-    if (width < 768) {
+    // let width;
+    const width = window.innerWidth;
+    
+    if (width <= 767) {
         return "mobile";
-    } else if (width < 1024) {
+    } else if (width <= 1024) {
         return "tablet";
     } else {
         return "desktop";
     }
+
+    // try {
+    //     if (window.top !== window.self) {
+    //         width = window.top.innerWidth;
+    //     } else {
+    //         width = window.innerWidth;
+    //     }
+    // } catch (e) {
+    //     width = window.innerWidth;
+    // }
+
+    // if (width < 768) {
+    //     return "mobile";
+    // } else if (width < 1024) {
+    //     return "tablet";
+    // } else {
+    //     return "desktop";
+    // }
 }
